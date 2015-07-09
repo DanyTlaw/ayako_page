b3bcc3f2b83ec315fc95443d040467ed22b4739e
Danytlaw <daniel.herzogfhnw@gmail.com>
2015-07-09 19:53:04 +0200

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-# 	heroku push ok same name now
+# 	heroku push ok same name now again
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
